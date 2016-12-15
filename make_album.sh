8b65d6964d2ef85ed62c66abd9e94f2fbbf53aed
norayr <norayr@arnet.am>
2016-12-15 19:30:11 +0400

    reading tab separated file.

--- make_album.sh
+++ make_album.sh
@@ -1,8 +1,21 @@
 
 TAGGER=id3v2
 YDL=youtube-dl
-
+LST=list.txt
 # check for prerequisites
 command -v $TAGGER >/dev/null 2>&1 || { echo "$TAGGER it's not installed.  Aborting." >&2; exit 1; }
 
 command -v $YDL >/dev/null 2>&1 || { echo "$YDL it's not installed.  Aborting." >&2; exit 1; }
+
+BAK_IFS=$IFS
+
+IFS=$'\t'
+
+while read LINK GROUP SONG
+do
+  #echo "$LINK $GROUP $SONG"
+  echo "$GROUP"
+done < list.txt
+
+IFS=$BAK_IFS
+

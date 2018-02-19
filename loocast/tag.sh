7e4e2f62c904a0a719be50e9f9bbce599470e41a
norayr <norayr@arnet.am>
2018-02-19 19:11:12 +0400

    l

---
+++ loocast/tag.sh
@@ -0,0 +1,26 @@
+
+set -x
+
+#first parameter - files extention to tag.
+
+if [ $# -eq 0 ]
+then
+  echo "no arguments supplied, using ogg"
+  ext="ogg"
+else
+  ext=$1
+  echo "will tag .$ext files" 
+fi
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for i in *.$ext
+do
+  echo $i
+  j=${i%.}
+  k=${j%-}
+  id3tool -t "$k" -a "$k" -r "Loocast" $i
+done
+
+IFS=$SAVEIFS
